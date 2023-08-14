35a56f193811364f2970c58adda1636e6dd818a1
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2023-08-14 20:25:44 +0530

    Localstorage + Collision Layout Added

--- app/demo/page.js
+++ app/demo/page.js
@@ -1,43 +1,94 @@
-"use client"
-import { Responsive, WidthProvider } from "react-grid-layout";
+"use client";
+import React, { useState } from "react";
+import { WidthProvider, Responsive } from "react-grid-layout";
 
-const ResponsiveGridLayout = WidthProvider(Responsive);
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
-import '../../node_modules/react-grid-layout/css/styles.css'
-import '../../node_modules/react-resizable/css/styles.css'
-import { useState } from "react";
+const Demo2 = () => {
+  const getFromLS = (key) => {
+    let ls = {};
+    try {
+      ls = JSON.parse(localStorage.getItem("rgl-8")) || {};
+    } catch (e) {}
 
-function Demo() {
-  const [layout2, setLayout2] = useState([
-    { i: "a", x: 0, y: 0, w: 1, h: 1, static: true },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 }
-  ])
+    return ls[key];
+  };
 
-  const layout1 = [
-    { i: "a", x: 0, y: 0, w: 1, h: 1 },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4, static: true },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 }
-  ];
+  const [layouts, setLayouts] = useState(() => {
+    const initialLayouts = getFromLS("layouts") || {};
+    return {
+      lg: initialLayouts.lg || [],
+      md: initialLayouts.md || [],
+      sm: initialLayouts.sm || [],
+      xs: initialLayouts.xs || [],
+      xxs: initialLayouts.xxs || [],
+    };
+  });
 
-  const layouts = 
-    {
-      lg: layout2, md: layout1, sm:layout1, xs: layout1
-    }
-    // const layouts = getLayoutsFromSomewhere();
-    return (
-      <ResponsiveGridLayout
-        className="layout"
-        layouts={layouts}
-        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-        rows={{ lg: 20, md: 10, sm: 6, xs: 4, xxs: 2 }}
-      >
-        <div key="a" className="bg-indigo-500">1</div>
-        <div key="b" className="bg-indigo-500">2</div>
-        <div key="c" className="bg-indigo-500">3</div>
-      </ResponsiveGridLayout>
+  const saveToLS = (key, value) => {
+    localStorage.setItem(
+      "rgl-8",
+      JSON.stringify({
+        ...getFromLS("layouts"),
+        [key]: value,
+      })
     );
-}
+  };
 
-export default Demo;
+  const onLayoutChange = (layout, layouts) => {
+    saveToLS("layouts", layouts);
+    setLayouts(layouts);
+  };
+
+  return (
+    <div className="h-screen border-2 border-red-500">
+      <ResponsiveReactGridLayout
+        className="border-2 border-red-500 h-screen"
+        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+        rowHeight={30}
+        layouts={layouts}
+        onLayoutChange={(layout, layouts) => onLayoutChange(layout, layouts)}
+        compactType={null}
+        preventCollision={true}
+      >
+        <div
+          key="1"
+          data-grid={{ w: 2, h: 3, minW: 2, minH: 3 }}
+          className="bg-red-500"
+        >
+          <span className="text">1</span>
+        </div>
+        <div
+          key="2"
+          data-grid={{ w: 2, h: 3, minW: 2, minH: 3 }}
+          className="bg-red-500"
+        >
+          <span className="text">2</span>
+        </div>
+        <div
+          key="3"
+          data-grid={{ w: 2, h: 3, minW: 2, minH: 3 }}
+          className="bg-red-500"
+        >
+          <span className="text">3</span>
+        </div>
+        <div
+          key="4"
+          data-grid={{ w: 2, h: 3, minW: 2, minH: 3 }}
+          className="bg-red-500"
+        >
+          <span className="text">4</span>
+        </div>
+        <div
+          key="5"
+          data-grid={{ w: 2, h: 3, minW: 2, minH: 3 }}
+          className="bg-red-500"
+        >
+          <span className="text">5</span>
+        </div>
+      </ResponsiveReactGridLayout>
+    </div>
+  );
+};
+
+export default Demo2;
