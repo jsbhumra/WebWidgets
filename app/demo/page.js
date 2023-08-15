8e5a007f47366c4d7de61b8123210c62417dab36
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2023-08-15 16:37:15 +0530

    Layout Bug Fixed

--- app/demo/page.js
+++ app/demo/page.js
@@ -1,44 +1,61 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { WidthProvider, Responsive } from "react-grid-layout";
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
-const Demo2 = () => {
+const Demo = () => {
   const getFromLS = (key) => {
     let ls = {};
-    try {
-      ls = JSON.parse(localStorage.getItem("rgl-8")) || {};
-    } catch (e) {}
-
+    if (localStorage) {
+      try {
+        ls = JSON.parse(localStorage.getItem("rgl-8")) || {};
+      } catch (e) {
+        console.log(e);
+      }
+    }
     return ls[key];
   };
 
-  const [layouts, setLayouts] = useState(() => {
-    const initialLayouts = getFromLS("layouts") || {};
-    return {
-      lg: initialLayouts.lg || [],
-      md: initialLayouts.md || [],
-      sm: initialLayouts.sm || [],
-      xs: initialLayouts.xs || [],
-      xxs: initialLayouts.xxs || [],
-    };
-  });
-
   const saveToLS = (key, value) => {
-    localStorage.setItem(
-      "rgl-8",
-      JSON.stringify({
-        ...getFromLS("layouts"),
-        [key]: value,
-      })
-    );
+    if (localStorage) {
+      localStorage.setItem(
+        "rgl-8",
+        JSON.stringify({
+          [key]: value,
+        })
+      );
+    }
   };
+
+  // const [layouts, setLayouts] = useState(() => {
+  //   const initialLayouts = getFromLS("layouts") || {};
+  //   return {
+  //     lg: initialLayouts.lg || [],
+  //     md: initialLayouts.md || [],
+  //     sm: initialLayouts.sm || [],
+  //     xs: initialLayouts.xs || [],
+  //     xxs: initialLayouts.xxs || [],
+  //   };
+  // });
+
+  const originalLayouts = getFromLS("layouts") || {};
+  const [layouts, setLayouts] = useState(
+    JSON.parse(JSON.stringify(originalLayouts))
+  );
+
+  useEffect(() => {
+    saveToLS("layouts", layouts);
+  }, [layouts]);
 
   const onLayoutChange = (layout, layouts) => {
-    saveToLS("layouts", layouts);
     setLayouts(layouts);
   };
+
+  // const onLayoutChange = (layout, layouts) => {
+  //   saveToLS("layouts", layouts);
+  //   setLayouts(layouts);
+  // };
 
   return (
     <div className="h-screen border-2 border-red-500">
@@ -54,35 +71,35 @@ const Demo2 = () => {
         <div
           key="1"
           data-grid={{ w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 }}
-          className="bg-red-500"
+          className="bg-white text-black"
         >
           <span className="text">1</span>
         </div>
         <div
           key="2"
           data-grid={{ w: 2, h: 3, x: 2, y: 0, minW: 2, minH: 3 }}
-          className="bg-red-500"
+          className="bg-white text-black"
         >
           <span className="text">2</span>
         </div>
         <div
           key="3"
           data-grid={{ w: 2, h: 3, x: 4, y: 0, minW: 2, minH: 3 }}
-          className="bg-red-500"
+          className="bg-white text-black"
         >
           <span className="text">3</span>
         </div>
         <div
           key="4"
           data-grid={{ w: 2, h: 3, x: 6, y: 0, minW: 2, minH: 3 }}
-          className="bg-red-500"
+          className="bg-white text-black"
         >
           <span className="text">4</span>
         </div>
         <div
           key="5"
           data-grid={{ w: 2, h: 3, x: 8, y: 0, minW: 2, minH: 3 }}
-          className="bg-red-500"
+          className="bg-white text-black"
         >
           <span className="text">5</span>
         </div>
@@ -91,4 +108,4 @@ const Demo2 = () => {
   );
 };
 
-export default Demo2;
+export default Demo;
