473dca978913b73a8c2152ea7822bf070b27cea6
jagjitsbhumra <jagjit.s.bhumra@gmail.com>
2023-08-18 21:12:03 +0530

    Few UI details added
     - Glassmorphism used
     - Neon color border given
     - Dark+Neon bg image

--- app/demo/page.js
+++ app/demo/page.js
@@ -1,4 +1,5 @@
 "use client";
+import Image from 'next/image';
 import React, { useState, useEffect } from "react";
 import { WidthProvider, Responsive } from "react-grid-layout";
 
@@ -41,8 +42,10 @@ const Demo = () => {
     setLayouts(layouts);
   };
 
+  console.log(layouts)
   return (
     <div className="h-screen">
+      <Image className="absolute h-screen top-0 start-0" height={1080} width={1920} src='/demobg.jpg' alt='BG-image'></Image>
       <ResponsiveReactGridLayout
         className="border-2 border-red-500 h-screen"
         cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
@@ -55,35 +58,35 @@ const Demo = () => {
         <div
           key="1"
           data-grid={{ w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 }}
-          className="bg-white text-black"
+          className="bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 border border-lime-500 border-2"
         >
           <span className="text">1</span>
         </div>
         <div
           key="2"
           data-grid={{ w: 2, h: 3, x: 2, y: 0, minW: 2, minH: 3 }}
-          className="bg-white text-black"
+          className="bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 border border-lime-500 border-2"
         >
           <span className="text">2</span>
         </div>
         <div
           key="3"
           data-grid={{ w: 2, h: 3, x: 4, y: 0, minW: 2, minH: 3 }}
-          className="bg-white text-black"
+          className="bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 border border-lime-500 border-2"
         >
           <span className="text">3</span>
         </div>
         <div
           key="4"
           data-grid={{ w: 2, h: 3, x: 6, y: 0, minW: 2, minH: 3 }}
-          className="bg-white text-black"
+          className="bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 border border-lime-500 border-2"
         >
           <span className="text">4</span>
         </div>
         <div
           key="5"
           data-grid={{ w: 2, h: 3, x: 8, y: 0, minW: 2, minH: 3 }}
-          className="bg-white text-black"
+          className="bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 border border-lime-500 border-2"
         >
           <span className="text">5</span>
         </div>
