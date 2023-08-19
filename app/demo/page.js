15d5e29f249083a91a1c3becfa70ffe306f97775
jagjitsbhumra <jagjit.s.bhumra@gmail.com>
2023-08-20 01:27:26 +0530

    UI refinements
     - Outline style and offset changed
     - Panel placeholder color changed
     - Resize handle customized
     - Image changed to bg-image of section
     - Some CSS values changed
     - Panel margin value changed
     - Panel now flex-styled, with items centred
     - Unnecessary commented code removed

--- app/demo/page.js
+++ app/demo/page.js
@@ -49,10 +49,9 @@ const Demo = () => {
 
   console.log(layouts)
   return (
-    <div className="h-screen">
-      <Image className="absolute h-screen top-0 start-0" height={1080} width={1920} src='/demobg.jpg' alt='BG-image'></Image>
+    <div className="min-h-screen h-auto bg-repeat bg-[url('/demobg.jpg')] bg-cover bg-center">
       <ResponsiveReactGridLayout
-        className="border-2 border-red-500 h-screen"
+        className="border-2 border-red-500 h-screen overflow-hidden"
         breakpoints = {{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
         cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
         rowHeight={30}
@@ -60,51 +59,17 @@ const Demo = () => {
         onLayoutChange={(layout, layouts) => onLayoutChange(layout, layouts)}
         compactType={null}
         preventCollision={true}
-        resizeHandles={['e','s']}
+        useCSSTransforms={true}
+        margin={[15,15]}
       >
         {currentLayout.map((box) => 
     <div
     key={box.i}
     data-grid={{ w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 }}
-    className="bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 border border-lime-500 border-2"
+    className="flex bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 outline outline-dashed outline-offset-[3.5px] outline-[3.5px] outline-lime-200 hover:outline-lime-500 active:outline-indigo-500 items-center justify-center cursor-grab active:cursor-grabbing"
   >
     <span className="text">{box.i}</span>
   </div>)}
-        {/* <div
-          key="1"
-          data-grid={{ w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 }}
-          className="bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 border border-lime-500 border-2"
-        >
-          <span className="text">1</span>
-        </div>
-        <div
-          key="2"
-          data-grid={{ w: 2, h: 3, x: 2, y: 0, minW: 2, minH: 3 }}
-          className="bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 border border-lime-500 border-2"
-        >
-          <span className="text">2</span>
-        </div>
-        <div
-          key="3"
-          data-grid={{ w: 2, h: 3, x: 4, y: 0, minW: 2, minH: 3 }}
-          className="bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 border border-lime-500 border-2"
-        >
-          <span className="text">3</span>
-        </div>
-        <div
-          key="4"
-          data-grid={{ w: 2, h: 3, x: 6, y: 0, minW: 2, minH: 3 }}
-          className="bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 border border-lime-500 border-2"
-        >
-          <span className="text">4</span>
-        </div>
-        <div
-          key="5"
-          data-grid={{ w: 2, h: 3, x: 8, y: 0, minW: 2, minH: 3 }}
-          className="bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10 border border-lime-500 border-2"
-        >
-          <span className="text">5</span>
-        </div> */}
       </ResponsiveReactGridLayout>
     </div>
   );
