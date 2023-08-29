1d03fafc027e86e8d6e646d817f220e67733eacc
Praneel Bora <praneelbora@gmail.com>
2023-08-30 00:20:45 +0530

    login signup

--- app/login/page.js
+++ app/login/page.js
@@ -5,29 +5,33 @@ import "./styles.css";
 import { EyeFilledIcon } from "./EyeFilledIcon";
 import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
 import { Button } from "@nextui-org/react";
-
+import Image from "next/image";
 export default function Login() {
-  const [isVisible, setIsVisible] = React.useState(false);
-
-  const toggleVisibility = () => setIsVisible(!isVisible);
-  return (
+    const [isVisible, setIsVisible] = React.useState(false);
+    function randomNum() {
+      return Math.ceil(Math.random() * 20);
+    }
+    const bgNum=randomNum()
+    const toggleVisibility = () => setIsVisible(!isVisible);
+    return (
     <>
-      <div className="body">
+    <div className="body flex justify-between pr-32 pb-44">
+        <div>
         <div className="textdiv">
-          <h4 className="text-3xl font-extrabold text1">LOG IN TO CONTINUE</h4>
-          <h2 className="text-6xl font-extrabold heading">Welcome back.</h2>
-          <p className="ms-1">
+        <h4 className="text-3xl font-extrabold text1">LOG IN TO CONTINUE</h4>
+        <h2 className="text-6xl font-extrabold heading">Welcome back.</h2>
+        <p className="ms-1">
             {" "}
             <span className="question">Become a member?</span>{" "}
             <a className="redirect" href="../signup">
-              Register
+            Register
             </a>
-          </p>
+        </p>
         </div>
         <form method="POST" action="../api/login/page">
-          <div className="flex flex-col gap-4 mt-10 form">
+        <div className="flex flex-col gap-4 mt-10 form">
             <div className="flex flex-wrap md:flex-nowrap mb-2 gap-4 inputlg">
-              <Input
+            <Input
                 classNames={{ label: "after:content-[']" }}
                 type="email"
                 name="email"
@@ -35,42 +39,45 @@ export default function Login() {
                 color="default"
                 label="Email "
                 isRequired
-              />
+            />
             </div>
             <Input
-              label="Password"
-              name="pass"
-              variant="bordered"
-              endContent={
+            label="Password"
+            name="pass"
+            variant="bordered"
+            endContent={
                 <button
-                  className="focus:outline-none"
-                  type="button"
-                  onClick={toggleVisibility}
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleVisibility}
                 >
-                  {isVisible ? (
+                {isVisible ? (
                     <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                  ) : (
+                ) : (
                     <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                  )}
+                )}
                 </button>
-              }
-              type={isVisible ? "text" : "password"}
-              className="inputlg  mb-6"
-              classNames={{ label: "after:content-[']" }}
-              isRequired
+            }
+            type={isVisible ? "text" : "password"}
+            className="inputlg  mb-6"
+            classNames={{ label: "after:content-[']" }}
+            isRequired
             />
             <Button
-              type="submit"
-              color="primary"
-              className="submitbtn"
-              radius="full"
-              variant="ghost"
+            type="submit"
+            color="primary"
+            className="submitbtn"
+            radius="full"
+            variant="ghost"
             >
-              Log In
+            Log In
             </Button>
-          </div>
+        </div>
         </form>
-      </div>
+        </div>
+        
+        <Image src={`/bgimages/img${bgNum}.svg`} width="700" height="500" className="bgimg"/>
+    </div>
     </>
-  );
+);
 }
