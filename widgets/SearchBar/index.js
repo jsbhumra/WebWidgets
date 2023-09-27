43b0a345816b9d3c8b7c739e85d06f3a581dd4f8
jsbhumra <jagjit.s.bhumra@gmail.com>
2023-09-27 23:15:04 +0530

    Searching updated :+1:
     - arrow key iteration enabled
     - valid URLs point to respective websites, others redirect to google
     - Clicking enter searches term or focused suggestion directly

--- widgets/SearchBar/index.js
+++ widgets/SearchBar/index.js
@@ -26,17 +26,64 @@ const SearchBar = () => {
     }
   }, [searchTerm]);
 
+  const isValidUrl = urlString=> {
+    try { 
+      return Boolean(new URL(urlString)); 
+    }
+    catch(e){ 
+      return false; 
+    }
+}
+
+  // var selectSearch =
+
+  var iterator = function(e) {
+    if(e.key == 'Enter'){
+      const focusElem = document.querySelector(':focus');
+      if(focusElem.tagName=="LI"){
+        if(isValidUrl(focusElem.innerHTML)) router.push(focusElem.innerHTML)
+        else router.push(`https://google.com/search?q=${focusElem.innerHTML}`)
+      } else if (focusElem.tagName=="INPUT"){
+        if(isValidUrl(searchTerm)) router.push(searchTerm)
+        else router.push(`https://google.com/search?q=${searchTerm}`)
+      }
+      
+      console.log(focusElem.tagName)
+      // router.push(`https://google.com/search?q=${focusElem.innerHTML}`)
+    }
+    if (e.key !== 'ArrowUp' && e.key !== 'ArrowDown') return;
+    const focusElem = document.querySelector(':focus');
+    const tabElements = [...document.querySelectorAll('#myinput, #searchList>li')];
+    const tabElementsCount = tabElements.length - 1;
+    if (!tabElements.includes(focusElem)) return;
+    e.preventDefault();
+    const focusIndex = tabElements.indexOf(focusElem);
+    let elemToFocus;
+    if (e.key === 'ArrowUp') elemToFocus = tabElements[focusIndex > 0 ? focusIndex - 1 : tabElementsCount];
+    if (e.key === 'ArrowDown') elemToFocus = tabElements[focusIndex < tabElementsCount ? focusIndex + 1 : 0];
+    elemToFocus.focus();
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', iterator);
+    return() => {
+      window.removeEventListener('keydown',iterator)
+    }
+  },[searchTerm])
+
   return (
     <div className="search-bar">
       <input
         type="text"
         placeholder="Search..."
+        id="myinput"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
+        tabIndex={1}
       />
-      <ul className="suggestions">
+      <ul className="suggestions" id="searchList">
         {suggestions.map((suggestion,i) => (
-          <li key={i} onClick={() => {router.push(`https://google.com/search?q=${suggestion}`)}}>{suggestion}</li>
+          <li key={i} tabIndex={i+4} value={suggestion} onClick={() => {if(!isValidUrl(suggestion)){router.push(`https://google.com/search?q=${suggestion}`)}else{router.push(suggestion)}}}>{suggestion}</li>
         ))}
       </ul>
     </div>
