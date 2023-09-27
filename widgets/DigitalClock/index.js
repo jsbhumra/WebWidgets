644c281b08b53254d95c5e69f3f6ba17a16a90ed
Praneel Bora <praneelbora@gmail.com>
2023-09-27 09:53:59 +0530

    digital clock bug fixed

--- widgets/DigitalClock/index.js
+++ widgets/DigitalClock/index.js
@@ -1,21 +1,38 @@
-"use client"
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState({
+    minutes: new Date().getMinutes(),
+    hours: new Date().getHours(),
+    seconds: new Date().getSeconds()
+  })
   
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setDate(new Date());
+      const date = new Date();
+      setTime({
+        minutes: date.getMinutes(),
+        hours: date.getHours(),
+        seconds: date.getSeconds()
+      })
     }, 1000)
 
     return () => clearInterval(intervalId);
   }, [])
 
+  const convertToTwoDigit = (number) => {
+    return number.toLocaleString('en-US', {
+      minimumIntegerDigits: 2
+    })
+  }
+
   return (
-    <p className='DigitalClock'>
-      {date.toLocaleTimeString()}
-    </p>
+    <div className='DigitalClock'>
+      <span>{convertToTwoDigit(time.hours)}:</span>
+      <span>{convertToTwoDigit(time.minutes)}:</span>
+      <span>{convertToTwoDigit(time.seconds)}</span>
+      <span>{time.hours >= 12 ? ' PM' : ' AM'}</span>
+    </div>
   );
 }
 
