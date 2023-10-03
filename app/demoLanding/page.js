19be840048c658ba377435470cd977f0b0697f92
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2023-10-03 13:59:41 +0530

    Demo Landing Page

---
+++ app/demoLanding/page.js
@@ -0,0 +1,211 @@
+"use client";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import React, { useState, useEffect, useRef } from "react";
+import { WidthProvider, Responsive } from "react-grid-layout";
+const AnalogClock = dynamic(() => import("@/widgets/AnalogClock"), {
+  ssr: false,
+});
+const DigitalClock = dynamic(() => import("@/widgets/DigitalClock"), {
+  ssr: false,
+});
+import SearchBar from "@/widgets/SearchBar";
+import { Button } from "@nextui-org/react";
+import toast, { Toaster } from "react-hot-toast";
+
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+function useWindowSize() {
+  const [size, setSize] = useState([window.innerHeight, window.innerWidth]);
+  useEffect(() => {
+    const handleResize = () => {
+      setSize([window.innerHeight, window.innerWidth]);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return size;
+}
+
+const Demo = () => {
+  const [screenHeight, screenWidth] = useWindowSize();
+
+  const screenSize = window.innerWidth;
+  let currentScreen, currCols;
+  switch (true) {
+    case screenSize > 1200:
+      currentScreen = "lg";
+      currCols = 12;
+      break;
+    case screenSize < 1200 && screenSize > 996:
+      currentScreen = "md";
+      currCols = 10;
+      break;
+    case screenSize < 996 && screenSize > 768:
+      currentScreen = "sm";
+      currCols = 6;
+      break;
+    case screenSize < 768 && screenSize > 480:
+      currentScreen = "xs";
+      currCols = 4;
+      break;
+
+    default:
+      currentScreen = "xxs";
+      currCols = 2;
+      break;
+  }
+
+  const getFromLS = (name, key) => {
+    let ls = {};
+    if (localStorage) {
+      try {
+        ls = JSON.parse(localStorage.getItem(name));
+        return ls[key];
+      } catch (e) {
+        // console.log(e);
+      }
+    } else {
+      {
+      }
+    }
+  };
+
+  const saveToLS = (name, key, value) => {
+    if (localStorage) {
+      localStorage.setItem(
+        name,
+        JSON.stringify({
+          [key]: value,
+        })
+      );
+    }
+  };
+
+  const originalWidgets = getFromLS("widgetStorage", "widgets");
+
+  const originalLayouts = getFromLS("layoutStorage", "layouts");
+
+  const onSaveOriginalLayouts = () => {
+    saveToLS("layoutStorage", "layouts", originalLayouts);
+  };
+
+  const onSaveOriginalWidgets = () => {
+    saveToLS("widgetStorage", "widgets", originalWidgets);
+  };
+
+  const [layouts, setLayouts] = useState(
+    JSON.parse(JSON.stringify(originalLayouts))
+  );
+
+  const [widgets, setWidgets] = useState(
+    JSON.parse(JSON.stringify(originalWidgets))
+  );
+
+  if (layouts === undefined) {
+    onSaveOriginalLayouts();
+    location.reload();
+  }
+
+  if (widgets === undefined) {
+    onSaveOriginalWidgets();
+    location.reload();
+  }
+
+  const [currentLayout, setCurrentLayout] = useState(
+    layouts[`${currentScreen}`]
+  );
+
+  const [currentWidget, setCurrentWidget] = useState(
+    widgets[`${currentScreen}`]
+  );
+
+  useEffect(() => {
+    setCurrentLayout(layouts[`${currentScreen}`]);
+    setCurrentWidget(widgets[`${currentScreen}`]);
+  }, []);
+
+  const breakPointChange = (newBreakPt) => {
+    setLayouts(getFromLS("layoutStorage", "layouts"));
+    setWidgets(getFromLS("widgetStorage", "widgets"));
+    setCurrentLayout(layouts[newBreakPt]);
+    setCurrentWidget(widgets[newBreakPt]);
+  };
+
+  return (
+    <div className="min-h-screen bg-repeat bgcol bg-cover bg-center">
+      <Toaster position="top-center" reverseOrder={false} />
+      <ResponsiveReactGridLayout
+        className="border-2 border-red-500 min-h-screen"
+        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+        rowHeight={30}
+        layouts={layouts}
+        compactType={null}
+        preventCollision={true}
+        useCSSTransforms={true}
+        margin={[15, 15]}
+        onBreakpointChange={(newBreakPt) => breakPointChange(newBreakPt)}
+        isResizable={false}
+        isDraggable={false}
+      >
+        {currentLayout.map((box) => {
+          console.log(currentWidget);
+          var thisWidget = currentWidget.filter((ele) => ele.i == box.i)[0]
+            .widget;
+          var boxheight = Math.floor(box.h * 30) + 15 * (box.h - 1);
+          var boxwidth =
+            Math.floor(box.w * (screenWidth / currCols)) + 15 * (box.w - 1);
+          return (
+            <div
+              key={box.i}
+              // Dynamic Values from Database for w, h, minW, minH.
+              data-grid={box}
+              className="group flex  bg-gray-950 rounded-md bg-clip-padding backdrop-filter backdrop-blur bg-opacity-10  items-center justify-center "
+            >
+              {/* Add Paddind to remove overlap betn widget and delete btn*/}
+              <div className="text w-full h-full pointer-events-none">
+                {/* {parentRef?.current[box.i]?.clientWidth} */}
+                {/* <Counter width={parentRef.current[box.i]?.clientWidth} height={parentRef.current[box.i]?.clientHeight} /> */}
+                {/* <p>Height: {boxheight}<br />Width: {boxwidth}</p> */}
+                {
+                  {
+                    empty: <></>,
+                    AnalogClock: (
+                      <AnalogClock
+                        width={Math.floor((9 / 10) * boxwidth)}
+                        height={boxheight}
+                        showSeconds={thisWidget.showSeconds}
+                        smooth={thisWidget.smooth}
+                      />
+                    ),
+                    DigitalClock: (
+                      <DigitalClock
+                        width={boxwidth}
+                        height={boxheight}
+                        clock24hr={thisWidget.clock24hr}
+                        showSeconds={thisWidget.showSeconds}
+                        vertical={thisWidget.vertical}
+                      />
+                    ),
+                    SearchBar: (
+                      <SearchBar
+                        width={Math.floor((9 / 10) * boxwidth)}
+                        height={boxheight}
+                        darkMode={thisWidget.darkMode}
+                      />
+                    ),
+                  }[thisWidget.name]
+                }
+              </div>
+            </div>
+          );
+        })}
+      </ResponsiveReactGridLayout>
+    </div>
+  );
+};
+
+export default Demo;
--- widgets/SearchBar/index.js
+++ widgets/SearchBar/index.js
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 import React, { useRef, useState, useEffect } from "react";
 import "./styles.css";
 import { useRouter } from "next/navigation";
 
-function SearchBar ({ width = 250, darkMode = true }) {
+function SearchBar({ width = 250, darkMode = true }) {
   const inputRef = useRef(null);
-  const router = useRouter()
-  const [value, setValue] = useState('')
+  const router = useRouter();
+  const [value, setValue] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [dark, setDark] = useState(darkMode);
@@ -16,83 +16,91 @@ function SearchBar ({ width = 250, darkMode = true }) {
   }, []);
 
   useEffect(() => {
-    setDark(darkMode)
-  },[darkMode])
+    setDark(darkMode);
+  }, [darkMode]);
 
   useEffect(() => {
     const fetchSuggestions = async () => {
       const response = await fetch(
-        `https://google.com/complete/search?client=chrome&q=${searchTerm}`);
+        `https://google.com/complete/search?client=chrome&q=${searchTerm}`
+      );
       const data = await response.json();
-      console.log(data[1])
-      if(data[1].length>0){
+      console.log(data[1]);
+      if (data[1].length > 0) {
         setSuggestions(data[1]);
       } else {
-        setSuggestions([])
+        setSuggestions([]);
       }
     };
 
     if (searchTerm.length > 0) {
       fetchSuggestions();
     } else {
-      setSuggestions([])
+      setSuggestions([]);
     }
   }, [searchTerm]);
 
-  const isValidUrl = urlString=> {
-    try { 
-      return Boolean(new URL(urlString)); 
+  const isValidUrl = (urlString) => {
+    try {
+      return Boolean(new URL(urlString));
+    } catch (e) {
+      return false;
     }
-    catch(e){ 
-      return false; 
-    }
-}
+  };
 
-  var selectSearch = function(e) {
-    const focusElem = document.querySelector(':focus');
-    const tabElements = [...document.querySelectorAll('#myinput, #searchList>li')];
+  var selectSearch = function (e) {
+    const focusElem = document.querySelector(":focus");
+    const tabElements = [
+      ...document.querySelectorAll("#myinput, #searchList>li"),
+    ];
     // const tabElementsCount = tabElements.length - 1;
-    if (!tabElements.includes(focusElem)){
+    if (!tabElements.includes(focusElem)) {
       setSuggestions([]);
-      setValue(searchTerm)
+      setValue(searchTerm);
     }
-  }
+  };
 
-  var iterator = function(e) {
-    if(e.key == 'Enter'){
-      const focusElem = document.querySelector(':focus');
-      if(focusElem.tagName=="LI"){
-        if(isValidUrl(focusElem.innerHTML)) router.push(focusElem.innerHTML)
-        else router.push(`https://google.com/search?q=${focusElem.innerHTML}`)
-      } else if (focusElem.tagName=="INPUT"){
-        if(isValidUrl(searchTerm)) router.push(searchTerm)
-        else router.push(`https://google.com/search?q=${searchTerm}`)
+  var iterator = function (e) {
+    if (e.key == "Enter") {
+      const focusElem = document.querySelector(":focus");
+      if (focusElem.tagName == "LI") {
+        if (isValidUrl(focusElem.innerHTML)) router.push(focusElem.innerHTML);
+        else router.push(`https://google.com/search?q=${focusElem.innerHTML}`);
+      } else if (focusElem.tagName == "INPUT") {
+        if (isValidUrl(searchTerm)) router.push(searchTerm);
+        else router.push(`https://google.com/search?q=${searchTerm}`);
       }
-      
+
       // console.log(focusElem.tagName)
       // router.push(`https://google.com/search?q=${focusElem.innerHTML}`)
     }
-    if (e.key !== 'ArrowUp' && e.key !== 'ArrowDown') return;
-    const focusElem = document.querySelector(':focus');
-    const tabElements = [...document.querySelectorAll('#myinput, #searchList>li')];
+    if (e.key !== "ArrowUp" && e.key !== "ArrowDown") return;
+    const focusElem = document.querySelector(":focus");
+    const tabElements = [
+      ...document.querySelectorAll("#myinput, #searchList>li"),
+    ];
     const tabElementsCount = tabElements.length - 1;
     if (!tabElements.includes(focusElem)) return;
     e.preventDefault();
     const focusIndex = tabElements.indexOf(focusElem);
     let elemToFocus;
-    if (e.key === 'ArrowUp') elemToFocus = tabElements[focusIndex > 0 ? focusIndex - 1 : tabElementsCount];
-    if (e.key === 'ArrowDown') elemToFocus = tabElements[focusIndex < tabElementsCount ? focusIndex + 1 : 0];
+    if (e.key === "ArrowUp")
+      elemToFocus =
+        tabElements[focusIndex > 0 ? focusIndex - 1 : tabElementsCount];
+    if (e.key === "ArrowDown")
+      elemToFocus =
+        tabElements[focusIndex < tabElementsCount ? focusIndex + 1 : 0];
     elemToFocus.focus();
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener('keydown', iterator);
-    window.addEventListener('click', selectSearch);
-    return() => {
-      window.removeEventListener('keydown',iterator)
-      window.removeEventListener('click',selectSearch)
-    }
-  },[searchTerm])
+    window.addEventListener("keydown", iterator);
+    window.addEventListener("click", selectSearch);
+    return () => {
+      window.removeEventListener("keydown", iterator);
+      window.removeEventListener("click", selectSearch);
+    };
+  }, [searchTerm]);
 
   return (
     <div className={`search-bar h-12 w-full relative mt-0`}>
@@ -103,25 +111,77 @@ function SearchBar ({ width = 250, darkMode = true }) {
         value={value}
         ref={inputRef}
         onChange={(e) => {
-          setValue(e.target.value)
-          setSearchTerm(e.target.value)
+          setValue(e.target.value);
+          setSearchTerm(e.target.value);
         }}
         // onBlur={() => setSuggestions([])}
         onFocus={() => setValue(searchTerm)}
         tabIndex={-1}
         autoComplete="off"
-        className={dark?"relative rounded-full w-full h-full ps-[5%] pe-[10%] text-white focus:outline-0 z-[100] bg-[#303134] placeholder:text-[#9aa0a6]":"relative rounded-full w-full h-full ps-[5%] pe-[10%] text-black focus:outline-0 z-[100] bg-white placeholder:text-gray-400"}
+        className={
+          dark
+            ? "relative rounded-full w-full h-full ps-[5%] pe-[10%] text-white focus:outline-0 z-[100] bg-[#303134] placeholder:text-[#9aa0a6]"
+            : "relative rounded-full w-full h-full ps-[5%] pe-[10%] text-black focus:outline-0 z-[100] bg-white placeholder:text-gray-400"
+        }
       />
-      {value!=''?<svg className={dark?"absolute z-[101] h-1/2 top-[25%] right-[3%] fill-[#9aa0a6] cursor-pointer":"absolute z-[101] h-1/2 top-[25%] right-[3%] fill-gray-400 cursor-pointer"} onClick={()=>{setSearchTerm('');setValue('')}} focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>:null}
-      <ul className={dark?"suggestions absolute w-full top-1/2 left-0 list-none overflow-y-auto z-[99] rounded-b-[1.5rem] pt-6 bg-[#303134]":"suggestions absolute w-full top-1/2 left-0 list-none overflow-y-auto z-[99] rounded-b-[1.5rem] pt-6 bg-white"} id="searchList">
-        
-      <hr className={dark?"absolute border-[#6c7075] rounded-full w-[95%] top-0 mt-6 left-1/2 -translate-x-1/2 z-[101]":"absolute border-gray-400 rounded-full w-[92.5%] top-0 mt-6 left-1/2 -translate-x-1/2 z-[101]"}/>
-        {suggestions.slice(0, 7).map((suggestion,i) => (
-          <li className={dark?"w-full px-[5%] p-[1%] focus:outline-0 focus:bg-[#3c4043] hover:bg-[#3c4043] cursor-pointer text-white":"w-full px-[5%] p-[1%] focus:outline-0 focus:bg-[#eeeeee] hover:bg-[#eeeeee] cursor-pointer text-black"} key={i} tabIndex={i+4} value={suggestion} onFocus={() => setValue(suggestion)} onClick={() => {if(!isValidUrl(suggestion)){router.push(`https://google.com/search?q=${suggestion}`)}else{router.push(suggestion)}}}>{suggestion}</li>
+      {value != "" ? (
+        <svg
+          className={
+            dark
+              ? "absolute z-[101] h-1/2 top-[25%] right-[3%] fill-[#9aa0a6] cursor-pointer"
+              : "absolute z-[101] h-1/2 top-[25%] right-[3%] fill-gray-400 cursor-pointer"
+          }
+          onClick={() => {
+            setSearchTerm("");
+            setValue("");
+          }}
+          focusable="false"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+        </svg>
+      ) : null}
+      <ul
+        className={
+          dark
+            ? "suggestions absolute w-full top-1/2 left-0 list-none overflow-y-auto z-[99] rounded-b-[1.5rem] pt-6 bg-[#303134] overflow-hidden"
+            : "suggestions absolute w-full top-1/2 left-0 list-none overflow-y-auto z-[99] rounded-b-[1.5rem] pt-6 bg-white overflow-hidden"
+        }
+        id="searchList"
+      >
+        <hr
+          className={
+            dark
+              ? "absolute border-[#6c7075] rounded-full w-[95%] top-0 mt-6 left-1/2 -translate-x-1/2 z-[101]"
+              : "absolute border-gray-400 rounded-full w-[92.5%] top-0 mt-6 left-1/2 -translate-x-1/2 z-[101]"
+          }
+        />
+        {suggestions.slice(0, 7).map((suggestion, i) => (
+          <li
+            className={
+              dark
+                ? "w-full px-[5%] p-[1%] focus:outline-0 focus:bg-[#3c4043] hover:bg-[#3c4043] cursor-pointer text-white"
+                : "w-full px-[5%] p-[1%] focus:outline-0 focus:bg-[#eeeeee] hover:bg-[#eeeeee] cursor-pointer text-black"
+            }
+            key={i}
+            tabIndex={i + 4}
+            value={suggestion}
+            onFocus={() => setValue(suggestion)}
+            onClick={() => {
+              if (!isValidUrl(suggestion)) {
+                router.push(`https://google.com/search?q=${suggestion}`);
+              } else {
+                router.push(suggestion);
+              }
+            }}
+          >
+            {suggestion}
+          </li>
         ))}
       </ul>
     </div>
   );
-};
+}
 
 export default SearchBar;
