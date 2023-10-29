89780bf3401b095986a164ac7434ec44d8003773
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2023-10-29 17:46:01 +0530

    Compare Backend Implemeted

--- app/sample/page.js
+++ app/sample/page.js
@@ -23,33 +23,6 @@ export default function add() {
     }
   }
 
-  async function getfromdb(userID) {
-    const response = await fetch(`/api/widget?userID=${userID}`, {
-      method: "GET",
-    });
-
-    const data = await response.json();
-    console.log(data);
-    if (!response.ok) {
-      throw new Error(data.message || "Something went wrong!");
-    }
-
-    return data;
-  }
-
-  async function compareData() {
-    var isTrue;
-    const dbdata = await getfromdb(userID);
-    const db1 = dbdata?.layouts;
-    const db2 = dbdata?.widgets;
-
-    const lsdata1 = getFromLS("layoutStorage", "layouts");
-    const lsdata2 = getFromLS("widgetStorage", "widgets");
-
-    isTrue = _.isEqual(db1, lsdata1) && _.isEqual(db2, lsdata2);
-    console.log(isTrue);
-  }
-
   return (
     <>
       <button
